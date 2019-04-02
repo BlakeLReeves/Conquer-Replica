1b4a9ccb1170adab192d33bb064853ac70c9f21f
blakereeves19 <blake.reeves19@gmail.com>
2019-04-02 12:27:38 -0500

    Added smooth scrolling through jQuery for cross browser support

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+$('.scroll').click(function(e) {
+    e.preventDefault();
+    $('body,html').animate({
+        scrollTop: $(this.hash).offset().top
+    }, 1500);
+});
+
 $(document).on('click', 'ul li', function () {
     $(this).addClass('active').siblings().removeClass('active');
 });
