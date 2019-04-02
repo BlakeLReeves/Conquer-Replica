12c9ca5e1ad297531930c00fd8048396b0c6ef06
blakereeves19 <blake.reeves19@gmail.com>
2019-04-02 15:17:56 -0500

    fixed animations in jQuery

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ $('.scroll').click(function(e) {
     }, 1500);
 });
 
+$(window).scroll(function() {
+    let scrollbarLocation = $(this).scrollTop();
+
+    $('.scroll').each(function() {
+        
+        let sectionOffset = $(this.hash).offset().top - 20;
+
+        if ( sectionOffset <= scrollbarLocation ) {
+            $(this).parent().addClass('active');
+            $(this).parent().siblings().removeClass('active');
+        }
+    })
+})
+
 $(document).on('click', 'ul li', function () {
     $(this).addClass('active').siblings().removeClass('active');
 });
